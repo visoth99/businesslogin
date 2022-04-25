53038468d713cb5f3f827b6c66a1da113d6a1acf
Visoth <blacksterteoz@gmail.com>
2022-04-25 11:11:08 -0700

    Initial commit

---
+++ businesslogin/src/main/java/com/visoth/businesslogin/BusinessloginApplication.java
@@ -0,0 +1,13 @@
+package com.visoth.businesslogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessloginApplication.class, args);
+	}
+
+}
---
+++ businesslogin/src/test/java/com/visoth/businesslogin/BusinessloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visoth.businesslogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
